912eeb194ebbfaece284e7d5a2b45e1e00ac5b23
Renato da Costa Castelo <rccastelo@gmail.com>
2023-03-28 19:42:16 -0300

    Bootcamp Dio Banco Pan Java Developer Desafio Triangulo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double A = leitor.nextDouble();
+        double B = leitor.nextDouble();
+        double C = leitor.nextDouble();
+        double maior;
+        double soma;
+        boolean triangulo;
+
+        maior = A;
+        soma = B + C;
+        if (B > maior) {
+            maior = B;
+            soma = A + C;
+        }
+        if (C > maior) {
+            maior = C;
+            soma = A + B;
+        }
+
+        triangulo = (soma > maior);
+
+        if (triangulo) {
+            double perimetro = (A + B + C);
+            System.out.printf("Perimetro = %.1f%n", perimetro);
+        } else {
+            double area = (((A + B) * C) / 2);
+            System.out.printf("Area = %.1f%n", area);
+        }
+
+        leitor.close();
+    }
 }
